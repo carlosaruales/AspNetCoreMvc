daba02559b6525b74acb90d767903fff9d182a1c
Andres ruales <carlosa.ruales@gmail.com>
2020-08-12 09:37:52 -0500

    Clase 6 - Se crea modelo para la escuela - Se pinta la información en la vista de escuela - Se incluye el espacio de nombres y se setea la información de la escuela dentro del controlador de escuela

--- AspNetCoreMvc/Controllers/EscuelaController.cs
+++ AspNetCoreMvc/Controllers/EscuelaController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCoreMvc.Models;
+using System;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +11,11 @@ namespace AspNetCoreMvc.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var escuela = new Escuela();
+            escuela.AnoFundacion = 2005;
+            escuela.Nombre = "Platzi";
+            escuela.EscuelaId = Guid.NewGuid().ToString();
+            return View(escuela);
         }
     }
 }
---
+++ AspNetCoreMvc/Models/Escuela.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace AspNetCoreMvc.Models
+{
+    public class Escuela
+    {
+        public string EscuelaId { get; set; }
+        public string Nombre { get; set; }
+        public int AnoFundacion { get; set; }
+    }
+}
