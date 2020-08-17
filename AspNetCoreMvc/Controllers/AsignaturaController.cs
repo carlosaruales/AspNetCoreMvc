067761de14b73ff7fa9279924c017be636152758
Andres ruales <carlosa.ruales@gmail.com>
2020-08-17 11:52:21 -0500

    10. Vistas para listar las asignaturas

---
+++ AspNetCoreMvc/Controllers/AsignaturaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AspNetCoreMvc.Controllers
+{
+    public class AsignaturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            Asignatura asignatura = new Asignatura
+            {
+                Nombre = "Inglés",
+                UniqueId = Guid.NewGuid().ToString()
+            };
+
+            return View(asignatura);
+        }
+    }
+}
