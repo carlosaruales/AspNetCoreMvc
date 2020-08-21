21c44430c7f19c4797ff3c030c9314bcc0e36026
Carlos <carlosa.ruales@gmail.com>
2020-08-20 22:49:32 -0500

    12. Mostrando listas de objetos

---
+++ AspNetCoreMvc/Controllers/AlumnoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using AspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AspNetCoreMvc.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Alumno> listAlumno = new List<Alumno>() {
+                new Alumno {
+                    Nombre = "Pedro",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new Alumno {
+                    Nombre = "Carlos",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new Alumno {
+                    Nombre = "Pepe",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new Alumno {
+                    Nombre = "Andrés",
+                    UniqueId = Guid.NewGuid().ToString()
+                },
+                new Alumno {
+                    Nombre = "Evanisto",
+                    UniqueId = Guid.NewGuid().ToString()
+                }
+            };
+
+            return View("MultiAlumno", listAlumno);
+        }
+    }
+}
