b934a6fec8a6f1da47efaa6487c96c1f1983d719
Carlos <carlosa.ruales@gmail.com>
2020-08-24 14:05:10 -0500

    15. Conectando la solución con Entity framework core

--- AspNetCoreMvc/Controllers/EscuelaController.cs
+++ AspNetCoreMvc/Controllers/EscuelaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AspNetCoreMvc.Models;
+using System.Linq;
 using System;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -8,14 +9,21 @@ namespace AspNetCoreMvc.Controllers
 {
     public class EscuelaController : Controller
     {
+        private EscuelaContext _context;
+
+        public EscuelaController(EscuelaContext Escuela){
+            _context = Escuela;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var escuela = new Escuela();
-            escuela.AñoDeCreación = 2005;
-            escuela.Nombre = "Platzi";
-            escuela.UniqueId = Guid.NewGuid().ToString();
-            ViewBag.otraPropiedad = "Soy una propiedad ViewBag :D";
+            var escuela = _context.Escuelas.FirstOrDefault();
+            //var escuela = new Escuela();
+            //escuela.AñoDeCreación = 2005;
+            //escuela.Nombre = "Platzi";
+            //escuela.UniqueId = Guid.NewGuid().ToString();
+            //ViewBag.otraPropiedad = "Soy una propiedad ViewBag :D";
             return View(escuela);
         }
     }
---
+++ AspNetCoreMvc/Models/EscuelaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreMvc.Models
+{
+    public class EscuelaContext: DbContext
+    {
+        public EscuelaContext(DbContextOptions<EscuelaContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Escuela> Escuelas { get; set; }
+        public DbSet<Asignatura> Asignaturas { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Evaluación> Evaluaciones { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
+    }
+}
